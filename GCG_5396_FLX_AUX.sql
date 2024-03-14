225bea5ad1d9e60698c01f7303b4b4c31e33c86c
Service Web <sw@gssmail.com>
2024-03-14 10:07:05 -0500

    Commit changes in C:\Websites\Git\emarlow\5396\GCG_5396_FLX_AUX.sql

---
+++ GCG_5396_FLX_AUX.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_5396_FLX_AUX"(
+ "RowID" CHAR(3),
+ "WO" CHAR(6),
+ "WO_SUF" CHAR(3),
+ "JobSuf" CHAR(10),
+ "ParentJob" CHAR(10),
+ "ParentPart" CHAR(20),
+ "FirstWcofParent" CHAR(20),
+ "Terminal" CHAR(3));
